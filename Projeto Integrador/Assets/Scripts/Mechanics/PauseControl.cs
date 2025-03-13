5b1e6cd3e0e353fa61d38728fc745e7060c85947
Leonardo Marques <leomquirino27@gmail.com>
2025-03-13 14:40:19 -0300

    Sistema de Pause feito

---
+++ Projeto Integrador/Assets/Scripts/Mechanics/PauseControl.cs
@@ -0,0 +1,62 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseControl : MonoBehaviour
+{
+    [Tooltip("Instancia o canvas do pause")]
+    public GameObject pauseCanvas; //Instancia o canvas do pause
+    public SceneLoader sceneLoader; //Instancia o sceneLoader
+    public String nextScene; //Nome da próxima cena
+    bool isPause = false; //Armazena se o jogo está pausado ou não
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //Garante que o jogo não estará pausado ao iniciar
+        EnableCanvas(false);
+    }
+
+    //Pausa o jogo e mostra o canva do Pause
+    void Pause(){
+        Time.timeScale = 0;
+
+        EnableCanvas(true);
+    }
+
+    //Despausa o jogo e esconde o canva do Pause
+    public void UnPause(){
+        Time.timeScale = 1;
+
+        EnableCanvas(false);
+    }
+
+    public void BackToMenu()
+    {
+        if(sceneLoader != null){
+            Time.timeScale = 1;
+            sceneLoader.Transition(nextScene);
+        }
+    }
+
+    //Responsavel por mostra ou esconder o canvas
+    void EnableCanvas(bool status){
+        if(pauseCanvas != null){
+            pauseCanvas.SetActive(status);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Executa o pause se o jogador pressionar a tecla ESC
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            isPause = !isPause;
+
+            if(isPause){
+                Pause();
+            }else {
+                UnPause();
+            }
+        }
+    }
+}
