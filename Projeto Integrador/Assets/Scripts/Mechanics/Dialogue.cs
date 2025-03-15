f91ef592169ffe6e8199cefe584328e9f9c63bd2
Leonardo Marques <leomquirino27@gmail.com>
2025-03-15 16:27:47 -0300

    Desenvolvimento do Sistema de NPC

---
+++ Projeto Integrador/Assets/Scripts/Mechanics/Dialogue.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+    public Sprite profile;
+    public string[] speechText;
+    public string actorName;
+
+    public LayerMask playerLayer;
+    public float radious;
+
+    private DialogueControl dc;
+    bool onRadious;
+
+    void Start()
+    {
+        dc = FindFirstObjectByType<DialogueControl>();
+    }
+
+    void FixedUpdate()
+    {
+        Interact();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Z)){
+            dc.Speech(profile, speechText, actorName);
+        }
+    }
+
+    public void Interact(){
+        Collider[] hits = Physics.OverlapSphere(transform.position, radious, playerLayer);
+
+        foreach (Collider hit in hits)
+        {
+            if (hit.CompareTag("Player")) // Mantém a verificação para evitar problemas futuros
+            {
+                onRadious = true;
+                break; // Evita chamadas repetidas
+            }else{
+                onRadious = false;
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, radious);
+    }
+}
---
+++ Projeto Integrador/Assets/Scripts/Mechanics/DialogueControl.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueControl : MonoBehaviour
+{
+    [Header("Components")]
+    public GameObject dialogueObj; //Referencia o game Object que irá aparecer o dialogo
+    public Image profile; //Referencia o objeto imagem que aparecera o sprite do personagem npc
+    public TextMeshProUGUI speechText; //Referencia o objeto texto que vai aparecer o discurso
+    public TextMeshProUGUI actorNameText; //Referencia o objeto texto que vai mostrar o nome do npc
+
+    [Header("Settings")]
+    public float typingSpeed; //Controla a velocidade que as palavras aparecerão
+    private string[] sentences;
+    private int index;
+
+    //Responsável por mostrar o discurso de um determinado npc
+    public void Speech(Sprite p, string[] txt, string actorName){
+        dialogueObj.SetActive(true); //Deixa amostra a caixa de dialago
+        profile.sprite = p; //Coloca o sprite do npc
+        sentences = txt; //Coloca o texto do discurso
+        actorNameText.text = actorName; //Coloca o nome do npc
+        StartCoroutine(TypeSentence());
+    }
+
+    IEnumerator TypeSentence(){
+        foreach(char letter in sentences[index].ToCharArray()){
+            speechText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentence(){
+        if(speechText.text == sentences[index]){
+            //ainda há textos
+            if(index < sentences.Length - 1){
+                index++;
+                speechText.text = "";
+                StartCoroutine(TypeSentence());
+            }else{ //lido quando acaba os textos
+                speechText.text = "";
+                index = 0;
+                dialogueObj.SetActive(false);
+            }
+        }
+    }
+}
